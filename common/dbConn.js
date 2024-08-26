f8666a02240dda95b6f6468c64a8534dc796005d
NAGARANI-SAI <nagarani101994@gmail.com>
2024-08-26 18:10:05 +0530

    get-std latest

--- common/dbConn.js
+++ common/dbConn.js
@@ -1,9 +1,9 @@
 var mongodb=require("mongodb")
 
 
-function getDB(){
+async function getDB(){
     const MongoClient=mongodb.MongoClient;
-    const server=MongoClient.connect("mongodb+srv://nagarani101994:nagarani1994@cluster0.sbzacep.mongodb.net/")
+    const server= await MongoClient.connect("mongodb+srv://nagarani101994:nagarani1994@cluster0.sbzacep.mongodb.net/")
     const db=server.db("sms")
     return db;
     
